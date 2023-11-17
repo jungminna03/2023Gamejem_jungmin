5a6bca4b9a515848f75449b87d696a4628f6eee8
redocean17 <124497200+redocean17@users.noreply.github.com>
2023-11-17 23:30:27 +0900

    합치기 수정

--- 2023Gamejam_jungmin/Assets/Scripts/Ore.cs
+++ 2023Gamejam_jungmin/Assets/Scripts/Ore.cs
@@ -15,11 +15,13 @@ public class Ore : MonoBehaviour
         if (ore != null && ore._count == _count && _nextOre != null && collision.transform.position.y > transform.position.y)
         {
             Vector3 middle = transform.position - collision.transform.position;
-            middle = middle / 2;
+            middle = (middle / 2) + collision.transform.position;
             Debug.Log(middle);
 
-            GameObject.Instantiate(_nextOre);
-            _nextOre.transform.position = middle;
+            GameObject go = GameObject.Instantiate(_nextOre);
+            go.transform.position = middle;
+            go.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
 
             GameObject.Destroy(collision.gameObject);
             GameObject.Destroy(gameObject);
