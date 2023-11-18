8b51fc5213abee3492605b8cf938ec97f88099a7
20181226leeyeongjae <145662323+20181226leeyeongjae@users.noreply.github.com>
2023-11-18 13:41:31 +0900

    fix

--- 2023Gamejam_jungmin/Assets/Scripts/Tutorial/TutorialScripts.cs
+++ 2023Gamejam_jungmin/Assets/Scripts/Tutorial/TutorialScripts.cs
@@ -14,45 +14,28 @@ public class TutorialScripts : MonoBehaviour
     private TextMeshProUGUI Text;
 
     public tutorialScripts[] ts = new tutorialScripts[0];
-    private int TutorialNum = 0;
-    private bool isRun = false;
-  
-    private void Start()
-    {
-        StartScripts(TutorialNum);
-    }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F) && !isRun)
-        {
-            Debug.Log("A");
-            
-        }
-    }
     public void StartScripts(int num)
     {
-        Debug.Log("StartSCripts()");
         StartCoroutine("PrintScripts", ts[num]);
     }
 
     IEnumerator PrintScripts(tutorialScripts dialogs)
     {
-        isRun = true;
         foreach (var dialog in dialogs.Dialogs)
         {
             Debug.Log("dialog");
             Text.text = dialog;
             while (true)
             {
+                yield return null;
                 if (Input.GetMouseButtonDown(0))
                 {
                     break;
                 }
             }
         }
-        TutorialNum++;
-        isRun = false;
-        yield return null;
+        Text.text = "";
+       
     }
 }
