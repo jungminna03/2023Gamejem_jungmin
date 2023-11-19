4af1c21f7a55cdfbf2aab3641de8413228839eaf
20181226leeyeongjae <145662323+20181226leeyeongjae@users.noreply.github.com>
2023-11-19 14:23:34 +0900

    fix

--- 2023Gamejam_jungmin/Assets/Scripts/ActiveOres.cs
+++ 2023Gamejam_jungmin/Assets/Scripts/ActiveOres.cs
@@ -7,12 +7,16 @@ public class ActiveOres : MonoBehaviour
 {
     [SerializeField]
     private Image[] oreImage;
-    private int myOres;
+    [SerializeField]
+    private GameObject Deadline;
+    [SerializeField]
+    private float[] flowers;
 
+    private int myOres;
     private void Awake()
     {
         myOres = MyOres.GetInstance.getMyOres();
-
+        Deadline.transform.position = new Vector2(0,flowers[myOres]);
         for (int i = 0; i < myOres; i++)
         {
             oreImage[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
